d50bb688f19d8201e49e1a2b2a01cb7d6c7ee59e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-28 18:06:51 +0100

    :sparkles: (commands) use config from commit command

--- src/commands/commit.ts
+++ src/commands/commit.ts
@@ -1,8 +1,14 @@
 import * as vscode from "vscode";
 import { CommandCallback } from ".";
+import { CzConfig } from "../config";
 
-export const commit = (context: vscode.ExtensionContext): CommandCallback => {
-  return () => {};
+export const commit = (
+  context: vscode.ExtensionContext,
+  czConfig: CzConfig
+): CommandCallback => {
+  return () => {
+    vscode.window.showInformationMessage(JSON.stringify(czConfig));
+  };
 };
 
 export default commit;
