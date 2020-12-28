707714d63cbb127e4a5f6d996403d8d1dc8af7b6
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-28 16:28:10 +0100

    :sparkles: (commands) default commit command

---
+++ src/commands/commit.ts
@@ -0,0 +1,8 @@
+import * as vscode from "vscode";
+import { CommandCallback } from ".";
+
+export const commit = (context: vscode.ExtensionContext): CommandCallback => {
+  return () => {};
+};
+
+export default commit;
