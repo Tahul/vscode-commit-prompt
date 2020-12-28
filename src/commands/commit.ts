3b35ddf5a4b13832c4a1f12de98b27a155c0cb7a
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-28 23:51:50 +0100

    :sparkles: (commands) commit command waiting or git integration

--- src/commands/commit.ts
+++ src/commands/commit.ts
@@ -1,13 +1,34 @@
 import * as vscode from "vscode";
 import { CommandCallback } from ".";
-import { CzConfig } from "../config";
+import { CzEmojiCodeConfig, CzEmojiConfig } from "../config";
+import ask from "../helpers/ask";
+import askOneOf from "../helpers/askOneOf";
+import { getQuestions } from "../helpers/getQuestions";
+import { API as GitAPI } from "../typings/git";
+import add from "./add";
 
 export const commit = (
+  git: GitAPI,
   context: vscode.ExtensionContext,
-  czConfig: CzConfig
+  czConfig: CzEmojiConfig,
+  czCodeConfig: CzEmojiCodeConfig
 ): CommandCallback => {
-  return () => {
-    vscode.window.showInformationMessage(JSON.stringify(czConfig));
+  const questions = getQuestions(czConfig, czCodeConfig);
+
+  return async () => {
+    let commitMessage: string = "";
+
+    for (const question of questions) {
+      if (question.type === "oneOf") {
+        commitMessage += await askOneOf(question, commitMessage);
+      }
+
+      if (question.type === "input") {
+        commitMessage += await ask(question, commitMessage);
+      }
+    }
+
+    console.log(commitMessage);
   };
 };
 
