329b852b625b19f225fe7340c412a2726b95f4cc
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-28 23:55:28 +0100

    :sparkles: (git) getGit helper

---
+++ src/helpers/getGit.ts
@@ -0,0 +1,18 @@
+import * as vscode from "vscode";
+import { API as GitAPI, GitExtension } from "../typings/git.d";
+
+export const getGit = (): GitAPI | undefined => {
+  const gitExtension = vscode.extensions.getExtension<GitExtension>(
+    "vscode.git"
+  )?.exports;
+
+  if (!gitExtension) {
+    return undefined;
+  }
+
+  const api: GitAPI = gitExtension.getAPI(1);
+
+  return api;
+};
+
+export default getGit;
