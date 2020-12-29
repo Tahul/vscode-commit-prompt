8156f063921245aa5a53870ee4c936bfecd6f295
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-29 03:24:41 +0100

    :sparkles: (commands) implement add command

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,16 +1,47 @@
-import { Address } from "cluster";
-import * as vscode from "vscode";
 import { CommandCallback } from ".";
 import askMultiple from "../helpers/askMultiple";
 import { getCurrentChanges } from "../helpers/getCurrentChanges";
-import { API as GitAPI, Change, Repository, Status } from "../typings/git";
+import { gitAdd } from "../helpers/gitAdd";
+import { gitRemove } from "../helpers/gitRemove";
+import { API as GitAPI, Change, Repository } from "../typings/git";
 
+/**
+ * Show a multi pick prompt with modified files.
+ * Picked = added to next commit
+ * Unpicked = not added to next commit
+ *
+ * @param git GitAPI
+ */
 export const add = (git: GitAPI): CommandCallback => {
   return async () => {
-    const picks: Change[] = await askMultiple(await getCurrentChanges(git));
+    let picks: Change[] = [];
 
+    try {
+      picks = await askMultiple(await getCurrentChanges(git));
+    } catch (e) {
+      console.log("Cancelling adding!");
+      return;
+    }
+
+    const repo: Repository = git.repositories[0];
+
+    // Get added changes
+    const { indexChanges } = repo.state;
+
+    // Remove unpicked files
+    for (const change of indexChanges) {
+      if (
+        !picks.find((pickedChange: Change): boolean => {
+          return pickedChange.uri.fsPath === change.uri.fsPath;
+        })
+      ) {
+        await gitRemove(git, repo, change);
+      }
+    }
+
+    // Add picked ones
     for (const pick of picks) {
-      const repo: Repository = git.repositories[0];
+      await gitAdd(git, repo, pick);
     }
   };
 };
