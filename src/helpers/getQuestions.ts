78d9e0ae0b45b7a8193b2737a7644ee8467bf167
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-29 03:31:07 +0100

    :sparkles: (get-questions) improve getQuestions helper

--- src/helpers/getQuestions.ts
+++ src/helpers/getQuestions.ts
@@ -1,51 +1,14 @@
-import * as vscode from "vscode";
-import { CzEmojiCodeConfig, CzEmojiConfig, CzEmojiType } from "../config";
-import { defaultTypes } from "./defaultTypes";
+import { CzEmojiConfig } from "../config";
+import defaultQuestions, { Question } from "./defaultQuestion";
 
-export interface Question {
-  name: string;
-  type: "oneOf" | "input";
-  placeHolder: string;
-  picks?: CzEmojiType[];
-  format?: string;
-}
-
-export const getQuestions = (
-  czConfig: CzEmojiConfig,
-  czCodeConfig: CzEmojiCodeConfig
-): Question[] => {
+/**
+ * Check if workspace config specifies a set of questions, otherwise use the default ones.
+ *
+ * @param czConfig CzEmojiConfig
+ */
+export const getQuestions = (czConfig: CzEmojiConfig): Question[] => {
   const questions: Question[] = !czConfig.questions
-    ? [
-        {
-          name: "type",
-          placeHolder: "Select the type of change you are committing (type)",
-          type: "oneOf",
-          picks: defaultTypes,
-        },
-        {
-          name: "scope",
-          placeHolder: "Specify a scope (scope)",
-          type: "input",
-          format: " ({value}) ",
-        },
-        {
-          name: "subject",
-          placeHolder: "Write a short description (subject)",
-          type: "input",
-        },
-        {
-          name: "body",
-          placeHolder: "Maybe provide a longer description (body)",
-          type: "input",
-          format: "\n\n{value}", // Break 2 lines for body
-        },
-        {
-          name: "issues",
-          placeHolder: "List any issue closed (issues)",
-          type: "input",
-          format: "\n\nIssues: {value}", // Break 2 lines for issues
-        },
-      ]
+    ? defaultQuestions
     : czConfig.questions;
 
   return questions;
