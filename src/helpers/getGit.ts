1bc04c0669445ba6b1fa74a9358b3da1eae2e0e4
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-29 03:30:50 +0100

    :pencil: (comment) comment getGit helper

--- src/helpers/getGit.ts
+++ src/helpers/getGit.ts
@@ -1,6 +1,9 @@
 import * as vscode from "vscode";
 import { API as GitAPI, GitExtension } from "../typings/git.d";
 
+/**
+ * Return the editor Git instance.
+ */
 export const getGit = (): GitAPI | undefined => {
   const gitExtension = vscode.extensions.getExtension<GitExtension>(
     "vscode.git"
